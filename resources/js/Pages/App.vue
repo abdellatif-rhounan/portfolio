d4cdff99c37836a8628754c2047a0651efbfb505
Abdellatif RHOUNAN <abdellatif.rhounan@gmail.com>
2024-02-10 17:32:35 +0000

    "PageHeader" Component Has Been Created :
    - Creation Of "PageHeader" Component & All its Related StyleSheet.

--- resources/js/Pages/App.vue
+++ resources/js/Pages/App.vue
@@ -1,9 +1,21 @@
 <template>
-	<h1>Portfolio App</h1>
+	<PageHeader />
 </template>
 
 <script>
+import PageHeader from "@/Components/PageHeader.vue";
+
 export default {
 	name: "App",
+
+	components: {
+		PageHeader,
+	},
 };
 </script>
+
+<style lang="scss">
+#app {
+	position: relative;
+}
+</style>
