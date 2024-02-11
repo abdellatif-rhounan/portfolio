c7ef6f0a3bdbac8cfa3953727a7b5463a62fa4a3
Abdellatif RHOUNAN <abdellatif.rhounan@gmail.com>
2024-02-11 15:23:20 +0000

    Scroll Bar | Favicon | NoScript :
    - Overriding Scroll Bar :
    	=> To Match The Theme.
    - Favicon Added.
    - NoScript Message Added :
    	=> For Browser With JavaScript Disabled.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,17 +1,24 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 
 <head>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-  <title>Portfolio. | Abdellatif Rhounan</title>
+  <title>Portfolio | Abdellatif Rhounan</title>
+  <link rel="icon" href="favicon.ico" />
   @vite(['resources/js/app.js', 'resources/css/app.scss'])
   @inertiaHead
 </head>
 
 <body>
   @inertia
+
+  <noscript>
+    <strong>
+      Sorry, but My Portfolio doesn't work properly without JavaScript enabled. Please enable it to continue.
+    </strong>
+  </noscript>
 </body>
 
 </html>
